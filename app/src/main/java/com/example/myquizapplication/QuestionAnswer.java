bd12f04c659ef3fac176ddca91c2235081090700
binita shrestha <stha.binita56@gmail.com>
2023-02-27 20:29:56 +0545

    myandroid project

---
+++ app/src/main/java/com/example/myquizapplication/QuestionAnswer.java
@@ -0,0 +1,30 @@
+package com.example.myquizapplication;
+
+public class QuestionAnswer {
+
+    public static String question[]={
+            "Control unit of a digital computer is often called the",
+            "Fifth generation digital computer will be",
+            "A Winchester disk is a",
+            "Real time computing is possible due to",
+            "Hard copy of a document can be obtained from"
+
+    };
+
+    public static String choices[][]={
+            {"ICS"," Clock","Nerve centre","All of these"},
+            {"Artificial intelligence","Extremely low cost","Very expensive","Versatility"},
+            {"Removable disk","Flexible disk","Disk stack","All of these"},
+            {"Accuracy","Storage capability","High speed","Versatility"},
+            {"Main memory","Card reader","CRT","All of these"}
+
+    };
+
+    public static String correctAnswer[]={
+            "Nerve centre",
+            "Artificial intelligence",
+            "Disk stack",
+            "High speed",
+            "Main memory"
+    };
+}
